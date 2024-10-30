48fe2aadc10abd9aa0c5e4cfde1e3d5d810c929b
chithanh1710 <chithanh171004@gmail.com>
2024-10-30 13:54:03 +0700

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 import React from "react";
 
 export default function loading() {
-  return (
-    <div className="w-full h-screen relative bg-[#FF99CC]">
-      <div className="absolute top-[30%] left-1/2 -translate-x-1/2 text-white flex flex-col justify-center items-center">
-        <Image
-          alt="Loading gif"
-          src="/images/loading.gif"
-          width={300}
-          height={200}
-          className="w-72 h-60 object-cover"
-        />
-        <div className="classic-2 mt-2">Chờ xíu nhe...</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full h-screen relative bg-[#FF99CC]">
+			<div
+				className="absolute top-[30%] left-1/2 -translate-x-1/2 text-white flex flex-col justify-center items-center">
+				<Image
+					alt="Loading gif"
+					src="/images/loading.gif"
+					width={300}
+					height={200}
+					className="w-72 h-60 object-cover"
+				/>
+				<div className="classic-2 mt-2">Chờ xíu nhe...</div>
+			</div>
+		</div>
+	);
 }
